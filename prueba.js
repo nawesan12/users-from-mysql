d3e89ac898ea57bdb33c71638cdd89f14a809fd9
Nahuel Santillan <84944031+nawesan12@users.noreply.github.com>
2023-06-22 11:46:55 -0300

    Update prueba.js

--- prueba.js
+++ prueba.js
@@ -1,19 +1,40 @@
+class UserCard extends HTMLElement {
+  constructor() {
+    super()
+
+    this.nombre = this.getAttribute("nombre") ?? "Usuario desconocido"
+    this.edad = this.getAttribute("edad") ?? "Edad desconocida"
+
+    const html = `
+      <li>
+        <p>Nombre: ${this.nombre}</p>
+        <p>Edad: ${this.edad}</p>
+      </li>
+    `
+
+    this.insertAdjacentHTML("beforeend", html)
+  }
+}
+
+customElements.define("user-card", UserCard)
+
+// ----------------------------------------------------------------------
+
 const listaDeUsuarios = document.getElementById("lista-de-usuarios")
 
-async function obtenerDatos() {
-  const respuesta = await fetch(`http://localhost:3000/usuarios`)
-  const datos = await respuesta.json()
+async function obtenerUsuarios() {
+  const res = await fetch("http://localhost:3000/usuarios")
+  const datos = await res.json()
 
-  // Devuelvo solo los pokemones
   return datos
 }
 
-obtenerDatos()
+obtenerUsuarios()
   .then((usuarios) => {
 
     usuarios.forEach((usuario) => {
       const itemDeUsuario = document.createElement("li")
-      itemDeUsuario.innerText = usuario.nombre
+      itemDeUsuario.innerHTML = `<user-card nombre="${usuario.nombre}" edad="${usuario.edad}"/>`
 
       listaDeUsuarios.appendChild(itemDeUsuario)
     })
